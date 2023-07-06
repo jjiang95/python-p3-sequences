9d95a83db2d82b341a203036cbf8945db6bf8957
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-07-06 14:24:19 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print(sequence)
+    else:
+        i = length - 2
+        while i > 0:
+            sequence.append(sequence[-1] + sequence [-2])
+            i -= 1
+        print(sequence)
